99c4b105ce0160061fa5834c37e0d5ad30169aa5
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2022-12-01 14:47:02 -0300

    Create A.cs

---
+++ A.cs
@@ -0,0 +1,15 @@
+public class A
+{
+    public int A1 {get; set;}
+    public float A2 {get; set;}
+
+    public void MA1()
+    {
+        Console.WriteLine("MA1");
+    }
+    
+    public void MA2()
+    {
+        Console.WriteLine("MA2");
+    }
+}
