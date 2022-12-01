91f5c8f5f420017300673e38c5e281b5aa935fe9
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2022-12-01 14:47:42 -0300

    Create B.cs

---
+++ B.cs
@@ -0,0 +1,16 @@
+public class B
+{
+    public int B1 {get; set;}
+    public float B2 {get; set;}
+
+    public void MB1()
+    {
+        Console.WriteLine("MB1");
+    }
+    
+    public void MB2()
+    {
+        Console.WriteLine("MB2");
+    }
+
+}
