45e2a1f7acb80fccfd6f1f0aff931e2113b86d9b
David de Jesus Costa <david.jc.br@hotmail.com>
2022-12-01 19:37:44 -0300

    Criação da Classe C

---
+++ C.cs
@@ -0,0 +1,16 @@
+public class C
+{
+    public int C1 {get; set;}
+    public float C2 {get; set;}
+
+    public void MC1()
+    {
+        Console.WriteLine("MC1");
+    }
+   
+    public void MC2()
+    {
+        Console.WriteLine("MC:2");
+    }
+
+}
