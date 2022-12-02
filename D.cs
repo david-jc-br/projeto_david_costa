9929413a71b159ee802e9aff1604d700c8799126
David de Jesus Costa <david.jc.br@hotmail.com>
2022-12-02 19:31:17 -0300

    ok

--- D.cs
+++ D.cs
@@ -12,10 +12,10 @@ public class D
     {
         return 5 + 1;
     }
-    
-    public void MD4()
+
+    public void MD3()
     {
-        Console.WriteLine("MD4");
+        Console.WriteLine("MD3");
     }
 
 }
